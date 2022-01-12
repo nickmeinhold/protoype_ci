7b019bfab9e33e8b8383ff1f19d3241a83f15a9e
Nick Meinhold <nick@enspyr.co>
2022-01-12 13:57:36 +1100

    first commit, copied from enspyrco/monorepo

---
+++ README.md
@@ -0,0 +1,29 @@
+# enspyrco monorepo
+
+*A [monorepo](https://en.wikipedia.org/wiki/Monorepo) of [Dart](https://dart.dev/) &amp; [Flutter](https://flutter.dev/) projects from [Enspyr](https://github.com/enspyrco), constituting a set of tools to facilitate & improve [adventuring](https://github.com/adventures-in/projects_summary).*
+
+[![codecov](https://codecov.io/gh/enspyrco/monorepo/branch/main/graph/badge.svg)](https://codecov.io/gh/enspyrco/monorepo)
+
+#### [adventures](packages/adventures/README.md)
+*Create, organise & discover adventures taking place in tech-world.*
+
+#### [flireator](packages/flireator/README.md)
+*A project creation tool for Flutter projects with a Firebase backend, stored on GitHub.*
+
+#### [redfire](packages/redfire/README.md)
+*A redux based state management & navigation solution, with prebuilt modules for Firebase features.*
+
+#### [redfire_test](packages/redfire_test/README.md)
+*Utilities to simplify testing in apps that use redfire.*
+
+#### [tech_world](packages/tech_world/README.md)
+*Adventures In Tech World, for fun and profit.*
+
+#### [the_process](packages/the_process/README.md)
+*A tool to automate "the process", a system for organising collaboration.*
+
+#### [ws_game_server](packages/ws_game_server/README.md)
+*Using shelf and shelf_web_socket for a serverless websocket server.*
+
+#### [ws_game_server_types](packages/ws_game_server_types/README.md)
+*Types for game objects sent via a ws_game_server.*
---
+++ packages/redfire/test/error-handling/unit-tests/widgets/add_problem_page_widget_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:redfire/src/problems/widgets/problem_page_view.dart';
+import 'package:redfire/types.dart';
+import 'package:redfire_test/redfire_test.dart';
+
+void main() {
+  group('ProblemPage', () {
+    final problem = ProblemInfo('Problem error message');
+
+    testWidgets('is added to widget tree', (WidgetTester tester) async {
+      final problemPageData = ProblemPageData(problem);
+
+      final state = ExampleAppState.init();
+      final updatedState =
+          state.copyWith(pages: state.pages.add(problemPageData));
+
+      final appWidget =
+          AppWidgetHarness.withFakeStore(initialState: updatedState).widget;
+
+      await tester.pumpWidget(appWidget);
+      await tester.pump();
+
+      final finder = find.byType(ProblemPageView);
+
+      expect(finder, findsOneWidget);
+    });
+
+    testWidgets('problem page appears after error caught',
+        (WidgetTester tester) async {
+      // The default services harness has mocked out plugins.
+      // One of the initial actions attempts to listen to auth state changes
+      // which throws in this context as the MockFirebaseAuth has no stub set.
+      final harness = AppWidgetHarness.withMockedPlugins(
+          ExampleAppState.init(), ServicesHarness());
+
+      await tester.pumpWidget(harness.widget);
+      await tester.pump();
+
+      expect(find.byType(ProblemPageView), findsOneWidget);
+    });
+  });
+}
